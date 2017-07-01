6c837a83d5a89a65b0f75869485c4aef16347f05
naomi789 <snjohnson789@gmail.com>
2017-06-30 17:23:31 -0700

    final version of js code

--- final_code_to_analyze_data.js
+++ final_code_to_analyze_data.js
@@ -4,6 +4,8 @@ const decisions = [
 ];
 const nodes = {};
 
+
+
 // Setup loop
 for(let i = 0; i < decisions.length; i++) {
   const instance = decisions[i];
@@ -41,87 +43,188 @@ _.forEach(nodes, (node, key) => {
   })
 })
 
+//score3 starting at 0
+_.forEach(nodes, (node, key) => {
+    node.score3zero = 0;
+  _.forEach(node.losses, (id) => {
+    node.score3zero -= Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+
+//score3 starting at score2
+_.forEach(nodes, (node, key) => {
+    node.score3 = node.score2;
+  _.forEach(node.losses, (id) => {
+    node.score3 -= Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+
+//score4  10 iterations (just adding) but using score2 over and over again…?
+/*_.forEach(nodes, (node, key) => {
+  node.score4 = node.score2;//start out where we left off with score2
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score4 += Math.abs(nodes[id].ratio - node.ratio);
+  })
+})*/
+
+//take the difference between the NEW truescores (instead of doing the ratio)
+//score 5
+_.forEach(nodes, (node, key) => {
+  node.score5 = 0;
+  _.forEach(node.wins, (id) => {
+    node.score5 += Math.abs(nodes[id].score2 - node.score2);
+  })
+})
+
+//score6
+_.forEach(nodes, (node, key) => {
+  node.score6 = node.score2;
+  node.score7 = node.score2;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
+_.forEach(nodes, (node, key) => {
+  _.forEach(node.wins, (id) => {
+    node.score6 += Math.abs(nodes[id].score7 - node.score7);
+  })
+})
+_.forEach(nodes, (node, key) => {
+  node.score7 = node.score6;
+})
 
 
 
-//print out desired results 
+
+//print out desire results 
 for(let i=0;i<Object.keys(nodes).length;i++) {
+  //console.log((nodes[(Object.keys(nodes)[i])].id)); //column0
   //console.log((nodes[(Object.keys(nodes)[i])].wins.length)); //column1
   //console.log((nodes[(Object.keys(nodes)[i])].losses.length)); //column2
   //console.log(nodes[(Object.keys(nodes)[i])].losses.length + nodes[(Object.keys(nodes)[i])].wins.length);//column3
   //console.log((nodes[(Object.keys(nodes)[i])].wins.length)/((nodes[(Object.keys(nodes)[i])].losses.length) + nodes[(Object.keys(nodes)[i])].wins.length));//column4 wins over matches
   //console.log((nodes[(Object.keys(nodes)[i])].wins.length)/((nodes[(Object.keys(nodes)[i])].losses.length)));//column5 wins over losses
   //console.log((nodes[(Object.keys(nodes)[i])].losses.length)/((nodes[(Object.keys(nodes)[i])].wins.length)));//column6 losses over wins 
-  console.log(nodes[(Object.keys(nodes)[i])].score2);
+  //console.log(nodes[(Object.keys(nodes)[i])].score2);
+  //console.log(nodes[(Object.keys(nodes)[i])].score3zero);
   //console.log(nodes[(Object.keys(nodes)[i])].score3);
   //console.log(nodes[(Object.keys(nodes)[i])].score4);
   //console.log(nodes[(Object.keys(nodes)[i])].score5);
+  console.log(nodes[(Object.keys(nodes)[i])].score6);
 }
-
-
-/*
-
-const nodes = {};
-
-// Setup loop
-for(let i = 0; i < decisions.length; i++) {
-  const instance = decisions[i];
-  nodes[instance.winner] = {
-    wins: [],
-    losses: [],
-    id: instance.winner,
-  };
-  nodes[instance.loser] = {
-    wins: [],
-    losses: [],
-    id: instance.loser,
-  };
-}
-
-
-// Wins vs losses
-for(let i = 0; i < decisions.length; i++) {
-  const instance = decisions[i];
-  nodes[instance.winner].wins.push(instance.loser);
-  nodes[instance.loser].losses.push(instance.winner);
-}
-
-// Calculate win/loss percentage
-_.forEach(nodes, (node, key) => {
-  node.ratio = node.wins.length / (node.wins.length + node.losses.length);
-  //node.lossRatio = node.losses.length / (node.wins.length + node.losses.length);
-})
-
-
-// Calculate for each of a nodes wins, the points it gains from its competitor?
-_.forEach(nodes, (node, key) => {
-  node.trueScore = 0;
-  _.forEach(node.wins, (id) => {
-    node.trueScore += Math.abs(nodes[id].ratio - node.ratio);
-  })
-})
-/*for(let i = 0; i < 3; i++)
-{
-  _.forEach(nodes, (node, key) => {
-  //node.trueScore = 0;
-  _.forEach(node.wins, (id) => {
-    node.trueScore += Math.abs(nodes[id].lossRatio - node.lossRatio);
-  })
-})
-}*/
-
-//loose points for loosing 
-/*_.forEach(nodes, (node, key) => {
-    node.score3 = 0;
-  _.forEach(node.losses, (id) => {
-    node.score3 -= Math.abs(nodes[id].ratio - node.ratio);
-  })
-})*/
-/*
-
-for(let i=0;i<Object.keys(nodes).length;i++) {
-  //console.log((nodes[(Object.keys(nodes)[i])].wins.length)/(nodes[(Object.keys(nodes)[i])].losses.length) + nodes[(Object.keys(nodes)[i])].wins.length);//trueScore
-  console.log(nodes[Object.keys(nodes)[i]].trueScore);
-}
-
-*/
